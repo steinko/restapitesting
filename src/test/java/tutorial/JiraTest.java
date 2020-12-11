9d368f6a76e7ea21c58a976f1059ddb26aa18d8e
steinko <stein_korsveien@msn.com>
2020-12-11 03:58:10 +0100

    add test of add comment

--- src/test/java/tutorial/JiraTest.java
+++ src/test/java/tutorial/JiraTest.java
@@ -3,6 +3,7 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
+import io.restassured.filter.session.SessionFilter;
 import static org.hamcrest.Matchers.*;
 import org.junit.jupiter.api.Test;
 
@@ -12,18 +13,60 @@ public class JiraTest {
 	@Test
 	public void shouldHaveATask() {
      	RestAssured.baseURI= "http://localhost:8080";
-	    String response=
+     	SessionFilter session = new SessionFilter();
+	 
 	    	given()
 	    	    .header("Content-Type","application/json")
 	            .body("{ \"username\": \"steinko\", \"password\": \"RoxyMusic11\" }")
+	            .filter(session)
 	       .when()
 	           .post("/rest/auth/1/session")
 	        .then()
 	            .assertThat()
-	                 .statusCode(200).body("session.name", equalTo("JSESSIONID"))
-	       .extract().response().asString();
-	
-	   JsonPath js=new JsonPath(response); //for parsing Json
-	   String sessionVallue=js.getString("session.value");
+	                 .statusCode(200).body("session.name", equalTo("JSESSIONID"));
+	       
+	   
+	//   String createRespons = 
+	//	  given()
+	//	      .header("Content-Type", "application/json").
+	//		   header("cookie","JSESSIONID=" + sessionValue).
+	//		   body("{\"fields\": {"
+	//		             +            "\"project\":"
+	//				     +               " {"
+	//		             +                    "\"key\": \"COOL\""+
+	//		                               "},"
+	//		             +             "\"summary\": \"Issue 5 for adding comment\","
+	//		             +             "\"description\": \"Creating my second bug\","
+	//		             +              "\"issuetype\": {"
+	//		             +                       "\"name\": \"Bug\""
+	//		             +                  "}"
+	//		             +      "}"
+	//		         + "}"                        
+	//		      )
+	//	 .when().
+	//		 post("/rest/api/2/issue")
+	//	 .then()
+	//		.statusCode(201)
+	//		.body("key", contains("COOL"))
+	//	  .extract().response().asString();
+	   
+	 //  JsonPath js1=new JsonPath(createRespons); //for parsing Json
+	 //  String key =js1.getString("key"); 
+	   
+	   given()
+	      .pathParam("key", "COOL-1")
+	      .header("Content-Type", "application/json")
+	      .filter(session)
+		  .body("{\r\n"
+		   		+ "    \"body\": \"This is a comment regarding the quality of the response.\"\r\n"
+		   		+ "}")
+	  .when().
+		 post("/rest/api/2/issue/{key}/comment")
+	  .then()
+	     .assertThat()
+		    .statusCode(201);
+					    
+					
+	   
 	}
 }
